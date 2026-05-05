31da6418f966ff69f1a8c57d5b97f49c67e05a77
yugjain991 <yugjain2515@gmail.com>
2026-05-06 02:28:31 +0530

    neflix clone

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
+
