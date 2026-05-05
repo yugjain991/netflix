7a53890bf84d22584af5bb94976cbf87cb60b607
yugjain991 <yugjain2515@gmail.com>
2026-05-06 04:23:39 +0530

    final tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily:{
-        mullish:["Mulish","sans-serif"],
-      },
-      
-    },
+    extend: {},
   },
   plugins: [],
 }
-
-
